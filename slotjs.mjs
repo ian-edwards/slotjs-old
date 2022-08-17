ed6c73470485385218e00c28de9563439c2f88a8
Ian Edwards <ian.philip.edwards@gmail.com>
2022-08-17 19:13:30 +0100

    updated documentation fixed weightIndex error

--- slotjs.mjs
+++ slotjs.mjs
@@ -2,6 +2,7 @@
  * generates a non-deterministic random natural integer in the interval [0, max)
  * @param {number} max maximum exclusive random value
  * @returns {number} random natural integer in the interval [0, max)
+ * @example randomNatural(8) // 4
  */
 const randomNatural = max => Math.floor(Math.random() * max)
 
@@ -9,6 +10,7 @@ const randomNatural = max => Math.floor(Math.random() * max)
  * returns the sum input weights
  * @param {number[]} weights input weights
  * @returns {number} weights sum
+ * @example sumWeights({ 1, 2, 3}) // 6
  */
 const sumWeights = weights => {
     let sum = 0
@@ -19,45 +21,49 @@ const sumWeights = weights => {
 }
 
 /**
- * 
- * @param {number[]} weights 
- * @param {function(number): number} random 
- * @returns {any}
+ * returns a random integer weight in the interval 0 to the sum of weights exclusive
+ * @param {number[]} weights input weights
+ * @param {function(number): number} random random natural integer generator
+ * @returns {number} random weight
+ * @example randomWeight({ 1, 2, 3 }, max => Math.floor(Math.random() * max)) // 5
  */
 const randomWeight = (weights, random) => random(sumWeights(weights))
 
 /**
- * 
- * @param {number[]} weights 
- * @param {number} weight 
- * @returns {number}
+ * returns the index of weight from weights cumulatively
+ * @param {number} weight index of this weight
+ * @param {number[]} weights input weights
+ * @returns {number} weight index
+ * @example weightIndex(4, {1, 2, 3}) // 2
  */
-const weightIndex = (weights, weight) => {
-    for (let i = 0, w = 0; i < items.length; i++) {
+const weightIndex = (weight, weights) => {
+    for (let i = 0, w = 0; i < weights.length; i++) {
         w += weights[i]
         if (weight < w) return i
     }
 }
 
 /**
- * 
- * @param {*} weights 
- * @param {*} random 
- * @returns 
+ * returns a random index from weights
+ * @param {number[]} weights input weights
+ * @param {function(number): number} random random natural integer generator
+ * @returns {number} random index
+ * @example randomIndex({ 1, 2, 3 }, max => Math.floor(Math.random() * max)) // 2
  */
-const randomIndex = (weights, random) => weightIndex(weights, randomWeight(weights, random))
+const randomIndex = (weights, random) => weightIndex(randomWeight(weights, random), weights)
 
 /**
- * 
- * @param {*} items 
- * @param {*} weights 
- * @param {*} random 
- * @returns 
+ * returns a random weighted item from input items
+ * @param {any[]} items input items
+ * @param {number[]} weights input weights
+ * @param {function(number): number} random random natural integer generator
+ * @returns {any} random item
+ * @example randomItem({ 'slot', 'js' }, { 1, 2 }, max => Math.floor(Math.random() * max)) // 'slot'
  */
 const randomItem = (items, weights, random) => items[randomIndex(weights, random)]
 
 const reels = [[
-    [ 1, 2, 3, 4, 5 ],
+    [ 9, 2, 3, 4, 5 ],
     [ 1, 2, 3, 4, 5 ],
     [ 1, 2, 3, 4, 5 ],
     [ 1, 2, 3, 4, 5 ]
@@ -70,4 +76,5 @@ const reels = [[
 
 const weights = [ 4, 5 ]
 
-console.log('hello world')
+const item = randomItem(reels, weights, randomNatural)
+console.log(item)
